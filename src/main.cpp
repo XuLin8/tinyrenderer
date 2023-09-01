c90243736b2f31ea4639a5249ad4331118fff5ff
xulin <xl@gameduchy.com>
2023-09-01 16:50:09 +0800

    Lec02 绘制实心三角形

--- src/main.cpp
+++ src/main.cpp
@@ -47,9 +47,33 @@ void line(const Vec2i& p1, const Vec2i& p2, TGAImage& image, TGAColor color)
 }
 
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
-    line(t0, t1, image, color);
-    line(t1, t2, image, color);
-    line(t2, t0, image, color);
+    //冒泡排序,实现 t0.y < t1.y < t2.y
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+    int total_height = t2.y - t0.y; 			//总高度
+    for (int y = t0.y; y <= t1.y; y++) { 		//下半部分绘制
+        int segment_height = t1.y - t0.y + 1;	//t0到t1的距离包括t1这一行 
+        float alpha = (float)(y - t0.y) / total_height; 	// alpha为y轴方向上，绘制的第y行占总高度的比例
+        float beta = (float)(y - t0.y) / segment_height; 	// 当心除0错，beta为y轴方向上，绘制的第y行占t1到t的高度的比例
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t0 + (t1 - t0) * beta;
+        if (A.x > B.x) std::swap(A, B); 		 	//从左到右绘制
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, y, color); 			// 注意，由于int强制转换t0.y+i！=A.y
+        }
+    }
+    for (int y = t1.y; y <= t2.y; y++) { 		//上半部分绘制
+        int segment_height = t2.y - t1.y + 1;
+        float alpha = (float)(y - t0.y) / total_height;
+        float beta = (float)(y - t1.y) / segment_height; // 当心除0错
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = t1 + (t2 - t1) * beta;
+        if (A.x > B.x) std::swap(A, B);
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, y, color); 			// 注意，由于int强制转换t0.y+i！=A.y
+        }
+    }
 }
 
 int main(int argc, char** argv) {
