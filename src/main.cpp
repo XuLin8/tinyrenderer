469f88a62b1d3a7b517b810f2e1d41bf534c9a2c
xulin <xl@gameduchy.com>
2023-09-01 15:34:56 +0800

    Lec01 Bresenham

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,42 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
-//Naive Line Drawing
+//Bresenham
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
-    for (float t = 0.; t < 1.; t += .01) {
-        int x = x0 + (x1 - x0) * t;
-        int y = y0 + (y1 - y0) * t;
-        image.set(x, y, color);
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) { // 如果斜率大于1，我们交换 x 和 y
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1) { // 如果起点在终点的右边，我们交换起点和终点
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2; // 用于判断是否需要增加 y 的错误值
+    int error2 = 0; // 当前的错误值
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+        if (steep) {
+            image.set(y, x, color); // 如果斜率大于1，交换 x 和 y 的坐标
+        }
+        else {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2 > dx) { // 如果错误累积超过 dx，需要增加 y 值
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
     }
 }
+
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
     line(13, 20, 80, 40, image, white);
-    image.flip_vertically(); // 左下角为图像原点
-    image.write_tga_file("Naive Line.tga");
+    image.flip_vertically();    // 原点在图像左下角
+    image.write_tga_file("Bresenham.tga");
     return 0;
 }
